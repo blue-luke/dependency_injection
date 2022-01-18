a36f7bf21aac0981dc121e6855b3f1050f70be19
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 19:00:50 +0000

    All tests isolated. Am using a different double syntax, will now implement the other  syntax, using let, as an exercise

--- ex1/spec/note_spec.rb
+++ ex1/spec/note_spec.rb
@@ -3,7 +3,8 @@ require 'note'
 describe Note do
   describe "#format" do
     it "returns a formatted note" do
-      note = Note.new("Tomorrow", "Buy bananas")
+      formatter_double = double :noteformatter, format: "Title: Tomorrow\nBuy bananas"
+      note = Note.new("Tomorrow", "Buy bananas", formatter_double)
       expect(note.display).to eq "Title: Tomorrow\nBuy bananas"
     end
   end
--- ex1/spec/noteformatter_spec.rb
+++ ex1/spec/noteformatter_spec.rb
@@ -2,7 +2,7 @@ require 'noteformatter'
 
 describe NoteFormatter do
   it 'formats a note' do
-  note_double = double :note, title: "Tomorrow", body: "Buy bananas"
+    note_double = double :note, title: "Tomorrow", body: "Buy bananas"
     noteformatter = NoteFormatter.new
     expect(noteformatter.format(note_double)).to eq "Title: Tomorrow\nBuy bananas"
   end
