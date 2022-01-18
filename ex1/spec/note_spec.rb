482e74a425c3a7424ad7ddaff070cb97ee7e8790
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-18 19:06:55 +0000

    Using let and allow syntax to create double, only in Note though, not NoteFormatter spec

--- ex1/spec/note_spec.rb
+++ ex1/spec/note_spec.rb
@@ -2,8 +2,9 @@ require 'note'
 
 describe Note do
   describe "#format" do
+  let(:formatter_double) { double NoteFormatter.new }
     it "returns a formatted note" do
-      formatter_double = double :noteformatter, format: "Title: Tomorrow\nBuy bananas"
+      allow(formatter_double).to receive(:format).and_return("Title: Tomorrow\nBuy bananas")
       note = Note.new("Tomorrow", "Buy bananas", formatter_double)
       expect(note.display).to eq "Title: Tomorrow\nBuy bananas"
     end
